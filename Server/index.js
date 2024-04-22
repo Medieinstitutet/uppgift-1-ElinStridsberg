c8935d978da6759e4868cb5b2177810ba7ecab1f
Elin Stridsberg <elin-stridsberg@hotmail.com>
2024-04-22 16:20:56 +0200

    some fixing

--- Server/index.js
+++ Server/index.js
@@ -6,9 +6,6 @@ let DatabaseConnection = require("./Database/DataBaseConnection");
 
 let url = 'mongodb://localhost:27017';
 
-// let dbConnection = new DatabaseConnection();
-// dbConnection.setUrl(url); 
-
 DatabaseConnection.getInstance().setUrl(url);
 
 let app = express();
@@ -18,7 +15,7 @@ app.use(express.urlencoded());
 
 app.use(cors());
 
-app.get("/", async (request, response) => {
+app.get("/orders", async (request, response) => {
 
     let orders = await DatabaseConnection.getInstance().getAllOrders();
 
@@ -26,19 +23,41 @@ app.get("/", async (request, response) => {
 }
 );
 
-app.get("/products", (request, response) => {
-    response.json([
-        {"id": 1, "Name": "produkt-1"}, 
-        {"id": 2, "Name": "produkt-2"}
-    ]);
+
+app.get("/products", async (request, response) => {
+    let products = await DatabaseConnection.getInstance().getProducts();
+
+    response.json(products);
 }
 );
+
 
 app.post("/create-order", async (request, response) => {
     //METODO: create customer
     let orderId = await DatabaseConnection.getInstance().saveOrder(request.body.lineItems, request.body.email);
 
     response.json({"id": orderId});
+
 });
+
+
+app.post("/products", async (request, response) => {
+
+    
+    let id = await DatabaseConnection.getInstance().createProduct();
+    await DatabaseConnection.getInstance().updateProduct(id, request.body);
+
+    response.json({"id": id});
+
+});
+
+app.post("/products/:id", async (request, response) => {
+
+    let id = await DatabaseConnection.getInstance().updateProduct(request.params.id, request.body);
+
+    response.json({"id": request.params.id});
+
+});
+
 
 app.listen(3000);
